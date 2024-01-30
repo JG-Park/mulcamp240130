9a6c8c97bb423b916a884dc4caf61a7dd5e8c0c1
JGP <jgp3620@gmail.com>
2024-01-30 17:07:02 +0900

    UPDATED : update file path

--- compress_data.py
+++ compress_data.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # 원본 데이터 파일 경로
-original_file_path = './input/orders.csv'
+original_file_path = './input/order_products__train.csv'
 
 # 새로운 데이터 파일 경로
-new_file_path = './input/new_orders.csv'
+new_file_path = './input/new/order_products__train.csv'
 
 # 랜덤 시드를 설정하여 매번 동일한 샘플을 얻을 수 있도록 함
 random_seed = 42
@@ -13,7 +13,7 @@ random_seed = 42
 df = pd.read_csv(original_file_path)
 
 # 데이터 랜덤 샘플링 (예: 50%의 비율로 샘플링)
-sampled_df = df.sample(frac=0.5, random_state=random_seed)
+sampled_df = df.sample(frac=0.1, random_state=random_seed)
 
 # 샘플링된 데이터를 새로운 파일로 저장
 sampled_df.to_csv(new_file_path, index=False)
